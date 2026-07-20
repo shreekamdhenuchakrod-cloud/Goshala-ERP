83f27e60a948607d952924b0de1d60f6043779f2
shree <shreekamdhenuchakrod@gmail.com>
2026-07-20 13:00:50 +0530

    Remove Firebase Hosting configuration

--- _write_cfg.mjs
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';  
-fs.writeFileSync('firebase.json', JSON.stringify({hosting:{public:'dist',ignore:['firebase.json'],rewrites:[{source:'**',destination:'/index.html'}]}},null,2));  
-fs.writeFileSync('.firebaserc', JSON.stringify({projects:{default:'skb-goshala-chakrod'}},null,2));  
-console.log('Files written!');  
